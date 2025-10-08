d7e1b34c5f1b9d24c059cdc546823e19cfec5141
Nike6po <sasha.prosolenko2006@mail.ru>
2025-10-08 21:46:20 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* Статистика Роналду */
+router.get('/ronaldo_statistic', function(req, res, next) {
+    res.send("<h1>Голы Роналду</h1>")
+});
+
+/* Награды Роналду */
+router.get('/ronaldo_awards', function(req, res, next) {
+    res.send("<h1>Страница Карамельки</h1>")
+});
+
+/* Семья Роналду */
+router.get('/ronaldo_family', function(req, res, next) {
+    res.send("<h1>Страница Компота</h1>")
+});
 
 module.exports = router;
