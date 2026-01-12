9f3a84ccf4825c0d8ad535acc4484c69b58c6701
Nike6po <sasha.prosolenko2006@mail.ru>
2026-01-12 03:18:29 +0300

    cr_8_2

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb://localhost/CR7')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var indexFootballers = require('./routes/footballers');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/footballers', indexFootballers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
