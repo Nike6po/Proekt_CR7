c55f57d4f90359fea6d81e188dc72a0290a098dd
Nike6po <sasha.prosolenko2006@mail.ru>
2026-01-12 19:37:52 +0300

    cr_9_5

---
+++ middlewares/createMenu.js
@@ -0,0 +1,13 @@
+var Footballer = require("../models/footballer").Footballer
+
+module.exports = async function(req,res,next){
+   res.locals.nav = []
+
+
+  var menu =  await Footballer.find({},{_id:0,title:1,nick:1});
+  console.log(menu);
+  if (menu.length != 0) {
+   res.locals.nav = menu;
+  }
+  next();
+}
