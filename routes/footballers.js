d7c5ca792749fb093d5ad4c5f47169563f35128b
Nike6po <sasha.prosolenko2006@mail.ru>
2026-01-12 03:25:56 +0300

    cr_8_3

--- routes/footballers.js
+++ routes/footballers.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Footballer = require('../models/footballer').Footballer;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,8 +8,17 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница Футболистов */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
+router.get("/:nick", async function(req, res, next) {
+    var footballers = await Footballer.find({nick: req.params.nick});
+    console.log(footballers)
+    if(!footballers.length) return next(new Error("Нет такого Футболиста в футбольном мире"))
+      var footballer = footballers[0];
+      res.render('footballer', {
+          title: footballer.title,
+          picture: footballer.avatar,
+          desc: footballer.desc
+       })
+
 });
 
 module.exports = router;
